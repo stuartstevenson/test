0e7fe49aa8027cab4f41e330c5952c420d1ba4f8
Stuart Stevenson <stuartjackstevenson@gmail.com>
2017-03-07 09:44:42 +0000

    Update ExampleController.java

--- src/main/java/com/example/helloboot/ExampleController.java
+++ src/main/java/com/example/helloboot/ExampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class ExamplController {
+public class Exampleontroller {
     @RequestMapping("/")
     public String index(Model model) {
         return "index";
